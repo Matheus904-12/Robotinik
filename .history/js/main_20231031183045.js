19d1b1e81dae66e2c8831814bcaeed355983e3db
Matheus904-17 <67837282+Matheus904-12@users.noreply.github.com>
2023-10-31 20:34:33 -0300

    terminado só que não

--- .history/js/main_20231031183045.js
+++ .history/js/main_20231031183045.js
@@ -1,11 +1,12 @@
 alert("Welcome to Official SimpL Esports");
 
+
 (function () {
+    
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
 })();
-
 const btn = document.querySelector("#btn");
 const sidebar = document.querySelector(".sidebar");
 const search = document.querySelector(".bx-search");
@@ -17,16 +18,3 @@ btn.onclick = function () {
 search.onclick = function () {
   sidebar.classList.toggle("active");
 }
-
-// Adicionando funcionalidade de compra
-const buyButtons = document.querySelectorAll(".buy-button");
-
-buyButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const product = button.parentElement;
-        const productName = product.querySelector("h3").innerText;
-        const productPrice = product.querySelector("p").innerText;
-        
-        alert(`Você comprou ${productName} por ${productPrice}`);
-    });
-});
